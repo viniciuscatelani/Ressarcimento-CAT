e30b515879b7ed7161edeeb6cb1bf104edb21fea
viniciuscatelani <66146042+viniciuscatelani@users.noreply.github.com>
2025-03-20 17:28:12 -0300

    .

--- src/etapas/gerar_tabela_2.py
+++ src/etapas/gerar_tabela_2.py
@@ -569,9 +569,9 @@ if cod_items_with_multiple_values.shape[0] > 0:
                            s3_key=f'Cat42/{nome_empresa.title()}/cods_a_verificar_{nome_empresa}_{cnpj}.xlsx', file_type='xlsx')
     sys.exit()
 
-# tabela_2 = tabela_2[(tabela_2['DATA'] >= '2023-01-01') & (tabela_2['DATA'] <= '2023-12-31')]
+tabela_2 = tabela_2[(tabela_2['DATA'] >= '2022-01-01') & (tabela_2['DATA'] <= '2022-12-31')]
 # data = tabela_2['DATA'].astype(str).iloc[0][:4]
-tabela_2 = tabela_2[(tabela_2['DATA'] >= '2020-01-01')]
+# tabela_2 = tabela_2[(tabela_2['DATA'] >= '2020-01-01')]
 tabela_2_filt = tabela_2[['CHV_DOC', 'DATA', 'CFOP', 'NUM_ITEM', 'COD_ITEM', 'MVA',
                     'IND_OPER', 'SUB_TIPO', 'QTD_CAT', 'QTD_EFD', 'ICMS_TOT', 'ICMS_TOT_SAIDA', 'VL_CONFR_0', 'COD_LEGAL',
                     'ALIQUOTA', 'VALOR', 'Valor Base Cálculo ICMS ST Retido Operação Anterior',
